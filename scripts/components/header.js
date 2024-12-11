0cfc7a46eb91249fee64cf1e28f5228f21589355
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2024-12-11 19:07:53 +0600

    header basic ui is done

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -1,6 +1,13 @@
- // get the header 
+// get the header
 
- const header = document.getElementsByTagName('header')[0];// the header we will work on 
- 
+const header = document.getElementsByTagName("header")[0]; // the header we will work on
 
- 
+export const setHeader = async () => {
+  const res = await fetch("/templates/header.html");
+  const stringified = await res.text();
+  const parser = new DOMParser();
+  const headerEntity = parser.parseFromString(stringified, "text/html");
+  const mainHeader = headerEntity.getElementById("main-header");
+  // the header that will be in the
+  header.appendChild(mainHeader.content);
+};
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
 import { setFooter } from "./components/footer.js";
+import { setHeader } from "./components/header.js";
 
+setHeader()
 setFooter();
