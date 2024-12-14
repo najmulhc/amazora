dc974ac67487087979a249c6daf2b035d9b0b9b9
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2024-12-14 18:28:48 +0600

    the auth is fully functioning

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,12 +1,11 @@
-// toggler for the checkboxes
-
-import { setState } from "./state/global.js";
+import { getState, setState } from "./state/global.js";
 import dataFetcher from "./util/dataFetcher.js";
 import generateUUID from "./util/generateUUID.js";
 
-// toggler for password
-
-// form handler
+const state = getState();
+if (state?.user) {
+  window.location.replace("/index.html");
+}
 
 const checkboxToggler = (checkboxId, inputId) => {
   const checkbox = document.getElementById(inputId);
@@ -23,10 +22,8 @@ const checkboxToggler = (checkboxId, inputId) => {
   });
 };
 
-const passwordInput = document.getElementById("input-password");
-
+const passwordInput = document.getElementById("password");
 const eyeIcon = document.getElementById("password-toggler");
-
 eyeIcon.addEventListener("click", () => {
   if (passwordInput.type == "password") {
     eyeIcon.setAttribute("src", "/assets/icons/auth/eye-closed.svg");
@@ -36,8 +33,14 @@ eyeIcon.addEventListener("click", () => {
     passwordInput.type = "password";
   }
 });
-
 checkboxToggler("custom-checkbox", "remember-me");
+
+const setUser = (userId) => {
+  setState({
+    user: userId,
+  });
+  window.location.replace("/index.html");
+};
 
 const signUp = async (formElem) => {
   const username = formElem.elements["username"].value;
@@ -47,7 +50,7 @@ const signUp = async (formElem) => {
     username,
     email,
     password,
-  }; 
+  };
   const userId = generateUUID();
 
   const userFetched = await dataFetcher("/rest/v1/users", "POST", {
@@ -59,15 +62,49 @@ const signUp = async (formElem) => {
   });
 
   if (createdUser.ok) {
-    // manage the global state
-    setState({
-      user: userId,
-    });
-    window.location.replace("/index.html");
+    setUser(userId);
   }
 };
+
+const signIn = async (elem) => {
+  const emailUsername = elem.elements["email-username"].value;
+  const password = elem.elements["password"].value;
+  const userInfo = {
+    email: undefined,
+    username: undefined,
+  };
+  if (emailUsername.includes("@")) {
+    userInfo.email = emailUsername;
+  } else {
+    userInfo.username = emailUsername;
+  }
+  const res = await dataFetcher(
+    `/rest/v1/user_auth?or=(username.eq.${userInfo.username},email.eq.${userInfo.email})`,
+    "GET"
+  );
+  const data = await res.json();
+
+  const user = data[0];
+  if (!user) {
+    document.getElementById("input-email-username").style.borderColor =
+      "#FF5630";
+  }
+  if (user?.password !== password) {
+    document.getElementById("input-password").style.borderColor = "#FF5630";
+  } else {
+    setUser(user.id);
+  }
+};
+
 const signUpForm = document.getElementById("sign-up");
-signUpForm.addEventListener("submit", async (event) => {
+if (signUpForm) {
+  signUpForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    await signUp(signUpForm);
+  });
+}
+const signInForm = document.getElementById("sign-in");
+signInForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-  await signUp(signUpForm);
+  await signIn(signInForm);
 });
