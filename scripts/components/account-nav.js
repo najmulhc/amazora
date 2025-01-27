5da5c1160969502408e2d9e98e432c1e35388149
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2025-01-27 21:28:30 +0600

    product page data fetching and data markup is done

--- scripts/components/account-nav.js
+++ scripts/components/account-nav.js
@@ -1,10 +1,15 @@
 import loadTemplate from "../util/loadTemplate.js";
-
+import {getState} from '../state/global.js'
+import dataFetcher from "../util/dataFetcher.js";
+const state = getState();
+const res = await dataFetcher(`rest/v1/user_details?id=eq.${getState().user}`, "GET");
+ const user = (await res.json())[0];
 async function initializeNav() {
   const navElem = document.getElementById("account-nav");
   const navTemplate = await loadTemplate("/templates/account-nav.html");
   const templateContainer = navTemplate.getElementsByTagName("template");
   const bottom = templateContainer[0].content.getElementById("nav-bottom");
+  templateContainer[0].content.getElementById("display-name").innerText = user.display_name;
   navElem.appendChild(bottom);
 }
 
--- scripts/pages/account/details.js
+++ scripts/pages/account/details.js
@@ -15,47 +15,49 @@ const setUserDetails = async () => {
 };
 
 const form = document.getElementById("account-form");
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const firstName = form.elements["first-name"].value;
-  const lastName = form.elements["last-name"].value;
-  const displayName = form.elements["display-name"].value;
-
-  const updatedPassword = form.elements["new-password"].value;
-  if (updatedPassword) {
-    const oldPassword = form.elements["old-password"].value;
-    if (!oldPassword) {
-      document.getElementById("old-password-error").innerText =
-        "Please enter the old password.";
-      return undefined;
+if(form) {
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const firstName = form.elements["first-name"].value;
+    const lastName = form.elements["last-name"].value;
+    const displayName = form.elements["display-name"].value;
+  
+    const updatedPassword = form.elements["new-password"].value;
+    if (updatedPassword) {
+      const oldPassword = form.elements["old-password"].value;
+      if (!oldPassword) {
+        document.getElementById("old-password-error").innerText =
+          "Please enter the old password.";
+        return undefined;
+      }
+      const user = await dataFetcher(
+        `rest/v1/user_auth?user_id=eq.${getState().user}&select=password`,
+        "GET"
+      );
+      const json = await user.json();
+      const { password } = json[0];
+      if (oldPassword !== password) {
+        document.getElementById("old-password-error").innerText =
+          "Given password is not correct.";
+        return undefined;
+      }
+  
+      const confirmPassword = form.elements["repeat-password"].value;
+      if (updatedPassword !== confirmPassword) {
+        document.getElementById("repeat-password-error").innerText =
+          "Passwords did not matched.";
+        return undefined;
+      }
+      await dataFetcher(`rest/v1/user_auth?user_id=eq.${state.user}`, "PATCH", {
+        password: updatedPassword,
+      });
     }
-    const user = await dataFetcher(
-      `rest/v1/user_auth?user_id=eq.${getState().user}&select=password`,
-      "GET"
-    );
-    const json = await user.json();
-    const { password } = json[0];
-    if (oldPassword !== password) {
-      document.getElementById("old-password-error").innerText =
-        "Given password is not correct.";
-      return undefined;
-    }
-
-    const confirmPassword = form.elements["repeat-password"].value;
-    if (updatedPassword !== confirmPassword) {
-      document.getElementById("repeat-password-error").innerText =
-        "Passwords did not matched.";
-      return undefined;
-    }
-    await dataFetcher(`rest/v1/user_auth?user_id=eq.${state.user}`, "PATCH", {
-      password: updatedPassword,
+  
+    await dataFetcher(`rest/v1/user_details?id=eq.${getState().user}`, "PATCH", {
+      first_name: firstName,
+      last_name: lastName,
+      display_name: displayName,
     });
-  }
-
-  await dataFetcher(`rest/v1/user_details?id=eq.${getState().user}`, "PATCH", {
-    first_name: firstName,
-    last_name: lastName,
-    display_name: displayName,
   });
-});
+}
 export { setUserDetails };
